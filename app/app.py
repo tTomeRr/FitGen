a64a56b6b4cbe00582d0d8b2024da88c69faa9fe
Tomer Cohen <tomerc248@gmail.com>
2024-01-16 14:31:44 +0200

    Added 404 route

--- app/app.py
+++ app/app.py
@@ -48,6 +48,10 @@ def strength():
 
     return render_template('strength.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'), 404
+
 
 if __name__ == "__main__":
     app.run()
